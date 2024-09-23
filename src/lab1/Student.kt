0d8a739239cac965159bff514c2c3ea5ac967bb1
Итачи <potroshitel2007@vk.com>
2024-09-24 01:56:41 +0300

    лр1 1-2: класс Student с геттерами и сеттерами и выводом

---
+++ src/lab1/Student.kt
@@ -0,0 +1,68 @@
+package lab1
+
+class Student(
+    id: UInt,
+    surname: String,
+    name: String? = null,
+    patronymic: String? = null,
+    phone: String? = null,
+    tg: String? = null,
+    email: String? = null,
+    giturl: String? = null
+) {
+    private val _id: UInt = id
+    private var _surname: String = surname
+    private var _name: String? = name
+    private var _patronymic: String? = patronymic
+    private var _phone: String? = phone
+    private var _tg: String? = tg
+    private var _email: String? = email
+    private var _giturl: String? = giturl
+
+    var surname: String
+        get() = _surname
+        set(value) {
+            _surname = value
+        }
+
+    var name: String?
+        get() = _name
+        set(value) {
+            _name = value
+        }
+
+    var patronymic: String?
+        get() = _patronymic
+        set(value) {
+            _patronymic = value
+        }
+
+    var phone: String?
+        get() = _phone
+        set(value) {
+            _phone = value
+        }
+
+    var tg: String?
+        get() = _tg
+        set(value) {
+            _tg = value
+        }
+
+    var email: String?
+        get() = _email
+        set(value) {
+            _email = value
+        }
+
+    var giturl: String?
+        get() = _giturl
+        set(value) {
+            _giturl = value
+        }
+
+    override fun toString(): String {
+        val fields = listOf(_id, _surname, _name, _patronymic, _phone, _tg, _email, _giturl)
+        return fields.joinToString(" ") { f -> f?.toString() ?: "" }
+    }
+}
