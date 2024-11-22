80ef60176370a8fc744570bff2adecbeaf813841
Избранное <egor.porf1@mail.ru>
2024-11-23 00:53:39 +0300

    #5-9 Конструктор из словаря и валидация полей

--- src/Student.kt
+++ src/Student.kt
@@ -1,16 +1,26 @@
+
 class Student(
     surname: String,
     name: String,
     lastname: String
 ) {
     private var _id : UInt? = null
-    private var _surname : String = surname
-    private var _name : String = name
-    private var _lastname : String = lastname
+    private var _surname : String
+    private var _name : String
+    private var _lastname : String
     private var _phone : String? = null
     private var _tg : String? = null
     private var _email : String? = null
     private var _git : String? = null
+
+    init {
+        checkSurname(surname)
+        checkName(name)
+        checkLastname(lastname)
+        _surname = surname
+        _name = name
+        _lastname = lastname
+    }
 
     constructor(
         id : UInt? = null,
@@ -26,6 +36,10 @@ class Student(
         name,
         lastname
     ) {
+        checkPhone(phone)
+        checkTg(tg)
+        checkEmail(email)
+        checkGit(git)
         _id = id
         _surname = surname
         _name = name
@@ -34,6 +48,54 @@ class Student(
         _tg = tg
         _email = email
         _git = git
+        validate()
+    }
+
+    fun validate() {
+        check(
+            this.hasGit() && this.hasContact()
+        ) {
+            "Необходимо указать "
+        }
+    }
+
+    fun hasGit() : Boolean {
+        return git != null
+    }
+
+    fun hasContact() : Boolean {
+        return listOf(phone, tg, email).any {it != null}
+    }
+
+    constructor(
+        params: Map<String, Any>
+    ) : this(
+        params.getOrDefault("id", null) as UInt?,
+        params["surname"] as String,
+        params["name"] as String,
+        params["lastname"] as String,
+        params.getOrDefault("phone", null) as String?,
+        params.getOrDefault("email", null) as String?,
+        params.getOrDefault("git", null) as String?
+    )
+
+    fun setContacts(
+        phone: String? = null,
+        tg: String? = null,
+        email: String? = null
+    ) {
+        if (phone != null) {
+            checkPhone(phone)
+            _phone = phone
+        }
+        if (tg != null) {
+            checkTg(tg)
+            _tg = tg
+        }
+        if (email != null) {
+            checkEmail(email)
+            _email = email
+        }
     }
 
     var id : UInt?
@@ -76,4 +138,56 @@ class Student(
         set(value) {
             _git = value
         }
+
+    companion object {
+        val PART_OF_NAME_REGEX = Regex("^[А-Яа-я]$")
+        val PHONE_REGEX = Regex("^(\\+7|8)\\s*\\(?\\d{3}\\)?\\s*\\d{3}[- ]?\\d{2}[- ]?\\d{2}\$\n")
+        val TG_REGEX = Regex("^(https://t\\.me/[a-zA-Z0-9_]+|tg://resolve\\?domain=[a-zA-Z0-9_]+)$\n")
+        val EMAIL_REGEX = Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}\$\n")
+        val GIT_REGEX = Regex("^(https://(www\\.)?(github\\.com|gitlab\\.com)/[a-zA-Z0-9._-]+/[a-zA-Z0-9._-]+)$\n")
+
+        fun checkSurname(surname: String) {
+            check(PART_OF_NAME_REGEX.matches(surname)) {
+                "Фамилия может содержать только буквы"
+            }
+        }
+        fun checkName(name: String) {
+            check(PART_OF_NAME_REGEX.matches(name)) {
+                "Имя может содержать только буквы"
+            }
+        }
+        fun checkLastname(lastname: String) {
+            check(PART_OF_NAME_REGEX.matches(lastname)) {
+                "Отчество может содержать только буквы"
+            }
+        }
+        fun checkPhone(phone: String?) {
+            check(
+                phone == null || PHONE_REGEX.matches(phone)
+            ) {
+                "Неправильный формат телефона"
+            }
+        }
+        fun checkTg(tg: String?) {
+            check(
+                tg == null || TG_REGEX.matches(tg)
+            ) {
+                "Неправильная ссылка на телеграм"
+            }
+        }
+        fun checkEmail(email: String?) {
+            check(
+                email == null || EMAIL_REGEX.matches(email)
+            ) {
+                "Неправильно задана почта"
+            }
+        }
+        fun checkGit(git: String?) {
+            check(
+                git == null || GIT_REGEX.matches(git)
+            ) {
+                "Неправильно задан Git"
+            }
+        }
+    }
 }
