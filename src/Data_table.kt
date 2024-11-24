981e4031bf463ed8897c25b544c719e8286588d5
Избранное <egor.porf1@mail.ru>
2024-11-24 13:34:26 +0300

    #2 Добавлены методы в Data_table

--- src/Data_table.kt
+++ src/Data_table.kt
@@ -1,3 +1,20 @@
 class Data_table(data: List<List<Any>>) {
     private var _data: List<List<Any>> = data
+
+    fun get(row: Int, column: Int) : Any {
+        require(row > 0 && column > 0) {
+            "Номер строки/столбца должен быть > 0"
+        }
+        require(row <= rows && column <= columns) {
+            "Таблица имеет меньший размер"
+        }
+
+        return _data[row][column]
+    }
+
+    val rows : Int
+        get() = _data.size
+
+    val columns : Int
+        get() = _data.first().size
 }
