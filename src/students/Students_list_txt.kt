19b4c5834bf90663fa0d28c8e0d801b84be96900
black-lemonov <egor.porf1@mail.ru>
2024-11-24 19:37:11 +0300

    ЛР3 #1 Класс Student_list_txt

---
+++ src/students/Students_list_txt.kt
@@ -0,0 +1,77 @@
+package students
+
+import template.Data_list
+import template.Data_list_student_short
+import java.io.File
+import java.io.FileNotFoundException
+import kotlin.math.max
+
+
+class Students_list_txt {
+    private var _data : MutableList<Student> = mutableListOf()
+
+    fun read_from_txt(filepath : String) {
+        val file = File(filepath)
+        if (!file.exists()) {
+            throw FileNotFoundException("Неверный путь к файлу: $filepath")
+        }
+        _data.clear()
+        file.readLines()
+            .forEach {
+                _data.add(Student(it))
+            }
+    }
+
+    fun write_to_txt(destpath: String) {
+        val file = File(destpath)
+        file.writeText(
+            _data.joinToString("\n") { it.toString() }
+        )
+    }
+
+    fun get(id: UInt) : Student {
+        return _data.first {it.getId() == id}
+    }
+
+    fun get_k_n_student_short_list(
+        page : Int, number : Int
+    ) : Data_list {
+        val prevPage = max(page-1, 0)
+        require(prevPage * number <= _data.size) {
+            "Нет страницы с номером $page"
+        }
+
+        return Data_list_student_short(_data
+            .drop(prevPage * number)
+            .take(number)
+            .map { Student_short(it) }
+        )
+    }
+
+    fun sortBySurnameAndInitials() {
+        _data.sort()
+    }
+
+    fun add(newStudent: Student) {
+        val maxId = _data.mapNotNull { it.getId() }.max()
+        add(newStudent, maxId + 1u)
+    }
+
+    private fun add(newStudent: Student, id: UInt) {
+        newStudent.setId(id)
+        _data.add(newStudent)
+    }
+
+    fun replaceWhere(newStudent: Student, id: UInt) {
+        deleteWhere(id)
+        add(newStudent, id)
+    }
+
+    fun deleteWhere(id: UInt) {
+        _data.removeIf { it.getId() == id }
+    }
+
+    fun get_student_short_count() : Int {
+        return _data.size
+    }
+}
