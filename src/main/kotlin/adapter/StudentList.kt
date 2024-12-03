0b1dd24d6b2fb27088d7c3564979799a57a2e265
black-lemonov <egor.porf1@mail.ru>
2024-12-03 16:13:02 +0300

    ЛР5 Добавлен интерфейс адаптера

---
+++ src/main/kotlin/adapter/StudentList.kt
@@ -0,0 +1,22 @@
+package adapter
+
+import students.Student
+import students.StudentShort
+import template.DataList
+
+
+interface StudentList {
+    fun get(id: Int) : Student
+
+    fun getByPage(page : Int, number : Int) : DataList<StudentShort>
+
+    fun orderBySurnameAndInitials()
+
+    fun add(student: Student)
+
+    fun replaceById(student: Student, id: Int)
+
+    fun removeById(id: Int)
+
+    fun countAll(): Int
+}
