8866658ba93323342ebdab719f4a6c513aecb8e1
black-lemonov <egor.porf1@mail.ru>
2024-12-17 11:47:12 +0300

    ЛР6 Добавлена область фильтрации

--- src/main/java/view/App.java
+++ src/main/java/view/App.java
@@ -1,11 +1,21 @@
 package view;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.util.Objects;
 
 
 public class App {
+    private static final JTextField nameField = new JTextField();
+    private static final JComboBox<String> gitComboBox = new JComboBox<>(new String[] {"Не важно", "Да", "Нет"});
+    private static final JTextField gitField = new JTextField();
+
+    private static final JTextField emailField = new JTextField();
+    private static final JComboBox<String> emailComboBox = new JComboBox<>(new String[] {"Не важно", "Да", "Нет"});
+    private static final JTextField phoneField = new JTextField();
+    private static final JComboBox<String> phoneComboBox = new JComboBox<>(new String[] {"Не важно", "Да", "Нет"});
+    private static final JTextField telegramField = new JTextField();
+    private static final JComboBox<String> telegramComboBox = new JComboBox<>(new String[] {"Не важно", "Да", "Нет"});
 
     public static void create() {
         SwingUtilities.invokeLater(() -> {
@@ -14,12 +24,58 @@ public class App {
             frame.setSize(854, 480);
 
             JTabbedPane tabbedPane = new JTabbedPane();
-            tabbedPane.add("Список студентов", new JLabel("Технические работы"));
+            tabbedPane.add("Список студентов", getStudentPanel());
             tabbedPane.add("Вкладка 2", new JLabel("Технические работы"));
             tabbedPane.add("Вкладка 3", new JLabel("Технические работы"));
 
             frame.add(tabbedPane);
             frame.setVisible(true);
+        });
+    }
+
+    private static JPanel getStudentPanel() {
+        JPanel panel = new JPanel(new BorderLayout());
+
+        addFilters(panel);
+
+        return panel;
+    }
+
+    private static void addFilters(JPanel panel) {
+        JPanel filterPanel = new JPanel(new GridLayout(5, 3));
+        filterPanel.setBorder(BorderFactory.createTitledBorder("Фильтры"));
+
+        filterPanel.add(new JLabel("Фамилия И.О."));
+        filterPanel.add(nameField);
+        filterPanel.add(new JLabel());
+
+        filterPanel.add(new JLabel("Телефон"));
+        filterPanel.add(phoneComboBox);
+        filterPanel.add(phoneField);
+        setFilter(phoneComboBox, phoneField);
+
+        filterPanel.add(new JLabel("Tg"));
+        filterPanel.add(telegramComboBox);
+        filterPanel.add(telegramField);
+        setFilter(telegramComboBox, telegramField);
+
+        filterPanel.add(new JLabel("Email"));
+        filterPanel.add(emailComboBox);
+        filterPanel.add(emailField);
+        setFilter(emailComboBox, emailField);
+
+        filterPanel.add(new JLabel("Git"));
+        filterPanel.add(gitComboBox);
+        filterPanel.add(gitField);
+        setFilter(gitComboBox, gitField);
+
+        panel.add(filterPanel, BorderLayout.NORTH);
+    }
+
+    private static void setFilter(JComboBox<String> comboBox, JTextField textField) {
+        textField.setEnabled(false);
+        comboBox.addActionListener(e -> {
+            textField.setEnabled(Objects.equals(comboBox.getSelectedItem(), "Да"));
         });
     }
 
