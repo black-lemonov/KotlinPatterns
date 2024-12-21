05cfc6a92a8eac9852e7bc25c0378ee636f351e1
black-lemonov <egor.porf1@mail.ru>
2024-12-21 17:46:58 +0300

    ЛР6 Реализация изменения полей студента и исправления в отображении

--- src/main/java/view/TablePanel.java
+++ src/main/java/view/TablePanel.java
@@ -6,8 +6,6 @@ import students.Student;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
-import javax.swing.table.TableRowSorter;
 import java.awt.*;
 import java.util.Objects;
 import java.util.function.Consumer;
@@ -69,7 +67,7 @@ public class TablePanel extends JPanel {
     }
 
     private void addTable() {
-        String[] columnNames = { "№", "ID", "Фамилия И.О.", "телефон", "tg", "email", "git" };
+        String[] columnNames = { "ID", "Фамилия И.О.", "телефон", "tg", "email", "git" };
         model = new DefaultTableModel(columnNames, 0) {
             @Override
             public boolean isCellEditable(int row, int column) {
@@ -78,9 +76,6 @@ public class TablePanel extends JPanel {
         };
         table = new JTable(model);
         table.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-
-        TableRowSorter<TableModel> sorter = new TableRowSorter<>(table.getModel());
-        table.setRowSorter(sorter);
 
         this.add(new JScrollPane(table), BorderLayout.CENTER);
     }
@@ -115,17 +110,23 @@ public class TablePanel extends JPanel {
 
         editButton.addActionListener(e -> {
             int selectedRow = table.getSelectedRow();
-            int id = (int) table.getValueAt(selectedRow, 0);
-            Student student = studentList.get(id);
-            if (student != null)
-                student.setName("Изменено");
-            refreshModel();
+            if (selectedRow >= 0) {
+                int id = (int) model.getValueAt(selectedRow, 0);
+                Student student = studentList.get(id);
+                if (student != null) {
+                    showForm(student, "Изменить студента", updatedStudent -> {
+                        studentList.update(updatedStudent, student.getId());
+                        JOptionPane.showMessageDialog(this, "Успешно!");
+                        refreshModel();
+                    });
+                }
+            }
         });
 
         deleteButton.addActionListener(e -> {
             int selectedRow = table.getSelectedRow();
             int id = (int) table.getValueAt(selectedRow, 0);
-            studentList.removeById(id);
+            studentList.remove(id);
             refreshModel();
         });
 
@@ -163,10 +164,10 @@ public class TablePanel extends JPanel {
         model.setRowCount(0);
 
         var tablePage = studentList.getByPage(CURRENT_PAGE, PAGE_SIZE).getData();
-        for (int row = 1; row <= tablePage.getRows(); row++ ) {
+        for (int row = 1; row <= tablePage.getRows(); ++row ) {
             var rowData = new Object[tablePage.getColumns()];
-            for (int col = 1; col <= tablePage.getColumns(); col++ ) {
-                rowData[col-1] = tablePage.get(row, col);
+            for (int col = 2; col <= tablePage.getColumns(); ++col ) {
+                rowData[col-2] = tablePage.get(row, col);
             }
             model.addRow(rowData);
         }
@@ -181,16 +182,16 @@ public class TablePanel extends JPanel {
 
     private static void showForm(Student student, String title, Consumer<Student> onSave) {
         JDialog dialog = new JDialog((Frame) null, title, true);
-        dialog.setSize(400, 300);
+        dialog.setSize(569, 320);
         dialog.setLayout(new GridLayout(8, 2));
 
         JTextField surnameField = new JTextField(student != null ? student.getSurname() : "");
         JTextField nameField = new JTextField(student != null ? student.getName() : "");
         JTextField lastnameField = new JTextField(student != null ? student.getLastname() : "");
         JTextField phoneField = new JTextField(student != null ? student.getPhone() : "");
-        JTextField tgField = new JTextField(student != null && student.getTg() != null ? student.getTg() : "");
-        JTextField emailField = new JTextField(student != null && student.getEmail() != null ? student.getEmail() : "");
-        JTextField gitField = new JTextField(student != null && student.getGit() != null ? student.getGit() : "");
+        JTextField tgField = new JTextField(student != null ? student.getTg() : "");
+        JTextField emailField = new JTextField(student != null ? student.getEmail() : "");
+        JTextField gitField = new JTextField(student != null ? student.getGit() : "");
 
         dialog.add(new JLabel("Фамилия"));
         dialog.add(surnameField);
@@ -235,7 +236,18 @@ public class TablePanel extends JPanel {
                 var email = emailField.getText().trim();
                 var git = gitField.getText().trim();
 
-                Student newStudent = student != null ? student : new Student(
+                Student newStudent;
+                if (student != null) {
+                    student.setSurname(surname);
+                    student.setName(name);
+                    student.setLastname(lastname);
+                    student.setPhone(phone.isEmpty() ? null : phone);
+                    student.setTg(tg.isEmpty() ? null : tg);
+                    student.setEmail(email.isEmpty() ? null : email);
+                    student.setGit(git.isEmpty() ? null : git);
+                    newStudent = student;
+                } else {
+                    newStudent = new Student(
                         1,
                         surname,
                         name,
@@ -244,7 +256,8 @@ public class TablePanel extends JPanel {
                         tg.isEmpty() ? null : tg,
                         email.isEmpty() ? null : email,
                         git.isEmpty() ? null : git
-                );
+                    );
+                }
                 newStudent.validate();
 
                 onSave.accept(newStudent);
