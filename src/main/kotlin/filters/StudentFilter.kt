3d576b030cbf4dd0d4007038deda69de9fee8a55
black-lemonov <egor.porf1@mail.ru>
2024-12-22 15:04:51 +0300

    Рефакторинг реализация фильтрации через класс

---
+++ src/main/kotlin/filters/StudentFilter.kt
@@ -0,0 +1,16 @@
+package filters
+
+import enums.SearchParam
+
+data class StudentFilter(
+    val surnameAndInitialsFilter: String,
+    val gitFilter: String,
+    val emailFilter: String,
+    val phoneFilter: String,
+    val tgFilter: String,
+
+    val gitSearch: SearchParam,
+    val phoneSearch: SearchParam,
+    val tgSearch: SearchParam,
+    val emailSearch: SearchParam,
+)
